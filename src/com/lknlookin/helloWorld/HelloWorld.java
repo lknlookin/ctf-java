5d6314c851c06c0f73332f8860cddf051447fd94
lknlookin <lknlookin@gmail.com>
2021-03-11 01:25:59 +0700

    first commit

---
+++ src/com/lknlookin/helloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.lknlookin.helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		bark();
+	}
+	
+	public static void bark() {
+		String dname = "Dugdig";
+		System.out.println("Dogname" + dname);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module com.lknlookin.helloWorld {
+}
