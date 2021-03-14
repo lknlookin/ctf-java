b0fdc3eb24dff8fc8c8183e4238e6b54ac50367d
lknlookin <lknlookin@gmail.com>
2021-03-14 23:54:38 +0700

    5

--- src/com/lknlookin/helloWorld/HelloWorld.java
+++ src/com/lknlookin/helloWorld/HelloWorld.java
@@ -3,25 +3,18 @@ package com.lknlookin.helloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		lab9();
-		lab10();
+		int a = returnVal();
+		System.out.println(a);
+		
+		notReturnVal();
 	}
 	
-	public static void lab9() {
-		for(int count = 20; count > 0; count--) {
-			if(count == 11) {
-				System.out.println(count);
-				continue;
-			}
-			System.out.println(count + " is not 11!");
-		}
+	public static int returnVal() {
+		System.out.println("This method is return val! see below");
+		return 29;
 	}
 	
-	public static void lab10() {
-		for(int i=1;i<=10;i++) {
-			if(i==2) continue;
-			
-			System.out.println(i);
-		}
+	public static void notReturnVal() {
+		System.out.println("This method is not return val!");
 	}
 }
