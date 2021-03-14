f685a0acb7474ad536d019c8aa34ea457af7fb2a
lknlookin <lknlookin@gmail.com>
2021-03-14 21:26:56 +0700

    2.1

--- src/com/lknlookin/helloWorld/HelloWorld.java
+++ src/com/lknlookin/helloWorld/HelloWorld.java
@@ -3,12 +3,25 @@ package com.lknlookin.helloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		bark();
-	}
-	
-	public static void bark() {
-		String dname = "Dugdig";
-		System.out.println("Dogname" + dname);
+		// single line comment
+		
+		/*
+		 * multiple line comment
+		 * multiple line comment
+		 * multiple line comment
+		 */
+		
+		int a = 10;
+		boolean boo = true;
+		String name = "lknlookin";
+		int[] arr = {0,1,2,3,4};
+		float f = 10f;
+		
+		System.out.println("This is a int " + a);
+		System.out.println("This is a boolean " + boo);
+		System.out.println("This is a name " + name);
+		System.out.println("This is a int array " + arr);
+		System.out.println("This is a float " + f);
+		
 	}
 }
