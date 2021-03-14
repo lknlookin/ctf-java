bf671fd257b6d16ca78d899dc24c65544aad2f30
lknlookin <lknlookin@gmail.com>
2021-03-14 22:43:00 +0700

    4.1

--- src/com/lknlookin/helloWorld/HelloWorld.java
+++ src/com/lknlookin/helloWorld/HelloWorld.java
@@ -3,28 +3,15 @@ package com.lknlookin.helloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int i = 20;
+		int score = 66;
+		boolean isHandSome = true;
 		
-		i++;
-		i++;
-		i++;
-		i++;
-		i++;
-		System.out.println(i);
-		
-		--i;
-		--i;
-		--i;
-		--i;
-		--i;
-		System.out.println(i);
-		
-		float f1 = 1.2f;
-		float f2 = 3.4f;
-		System.out.println((f1 == 1.2f) && (f2 != 2.3f));
-		
-		char a = 'a';
-		char b = 'b';
-		System.out.println((a == 'c') || (b == 'b'));
+		if(score >= 80 || isHandSome) {
+			System.out.println("Good");
+		}else if(score >= 50) {
+			System.out.println("normal");
+		}else {
+			System.out.println("fail");
+		}
 	}
 }
