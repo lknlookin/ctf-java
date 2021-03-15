8fb2d035eaa3519b1d1549c34916627f3434b057
lknlookin <lknlookin@gmail.com>
2021-03-15 22:55:53 +0700

    6

--- src/com/lknlookin/helloWorld/HelloWorld.java
+++ src/com/lknlookin/helloWorld/HelloWorld.java
@@ -3,12 +3,8 @@ package com.lknlookin.helloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		bark();
-	}
-	
-	public static void bark() {
-		String dname = "Dugdig";
-		System.out.println("Dogname" + dname);
+		char[] chars = {'a', 'b', 'c'};
+		String str = new String(chars);
+		System.out.println(str);
 	}
 }
